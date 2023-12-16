dd8841f7436e89dd2552f01801a51e451f10c0b3
denfasyah <denfasyah@gmail.com>
2023-12-16 23:36:51 +0700

    setup

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Junior Developer Community | Forum</title>
+</head>
+<body>
+{{-- Blue : #178EFC, #1077D5, #07E1FF --}}
+{{-- Abu : #D2D2D2, 969696  --}}
+{{-- Background : EBEBEB --}}
+<div class="m-6">
+    <div class="avatar">
+        <div class="bg-[#178EFC] mask mask-hexagon w-28">
+            <div class="w-24 mask mask-hexagon mx-auto mt-2">
+                <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>                                         
