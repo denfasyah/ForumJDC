85b80a5853faf1ab01986c3a696cfd35081f5951
denfasyah <denfasyah@gmail.com>
2024-03-07 21:00:29 +0700

    update question page

--- resources/views/question/answer/index.blade.php
+++ resources/views/question/answer/index.blade.php
@@ -4,21 +4,24 @@
   rel="stylesheet"
 />
 <div class="container">
-  <div class="flex flex-wrap mt-6 -mx-3 scroll-behavior">
-    <div class="w-full px-3 mt-5 lg:mt-0 mb-6 lg:mb-0 lg:w-7/12 lg:flex-none">
-      <!-- quest -->
+  <div class="flex justify-center flex-wrap mt-6 -mx-3 scroll-behavior">
+    <!-- quest -->
+    <div class="w-full pb-5 px-3 mt-5 lg:mt-0 mb-6 lg:mb-0 lg:flex-none">
       <div
         class="flex flex-col min-w-0 break-words bg-white shadow-soft-xl rounded-2xl bg-clip-border"
       >
-        <div class="flex-auto p-4">
+        <div class="flex-auto px-4 pb-4">
           <div class="flex flex-wrap -mx-3">
             <div class="max-w-full px-3 lg:w-1/2 lg:flex-none">
               <div class="flex flex-col h-full">
                 <div class="avatar flex items-center">
-                  <div class="mask mask-circle w-7 lg:w-12 md:w-14 sm:w-8">
+                  <div class="mask mask-circle w-10 lg:w-14 md:w-14 sm:w-8">
                     <img src="{{ asset('icon/anime.png') }}" />
                   </div>
-                  <h5 class="font-bold ml-5">Adent</h5>
+                  <div class="flex flex-col gap-2">
+                    <h5 class="font-bold mt-4 ml-5 h-4">Adent</h5>
+                    <span class="text-xs ml-5">10.00</span>
+                  </div>
                 </div>
                 <p class="mb-1 font-semibold">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -44,19 +47,22 @@
       <div
         class="mt-5 flex flex-col min-w-0 break-words bg-white shadow-soft-xl rounded-2xl bg-clip-border"
       >
-        <div class="flex-auto p-4">
+        <div class="flex-auto px-4 pb-4">
           <div class="flex flex-wrap -mx-3">
             <div class="max-w-full px-3 lg:w-1/2 lg:flex-none">
               <div class="flex flex-col h-full">
                 <div class="avatar flex items-center">
-                  <div class="mask mask-circle w-7 lg:w-12 md:w-14 sm:w-8">
+                  <div class="mask mask-circle w-10 lg:w-14 md:w-14 sm:w-8">
                     <img src="{{ asset('icon/anime.png') }}" />
                   </div>
-                  <h5 class="font-bold ml-5">Adent</h5>
+                  <div class="flex flex-col gap-2">
+                    <h5 class="font-bold mt-4 ml-5 h-4">Adent</h5>
+                    <span class="text-xs ml-5">10.00</span>
+                  </div>
                 </div>
                 <p class="mb-1 font-semibold">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Similique, laudantium?
+                  Similique, laudantium
                 </p>
               </div>
             </div>
@@ -67,57 +73,50 @@
       <div
         class="mt-2 flex flex-col min-w-0 break-words bg-white shadow-soft-xl rounded-2xl bg-clip-border"
       >
-        <div class="flex-auto p-4">
+        <div class="flex-auto px-4 pb-4">
           <div class="flex flex-wrap -mx-3">
             <div class="max-w-full px-3 lg:w-1/2 lg:flex-none">
               <div class="flex flex-col h-full">
                 <div class="avatar flex items-center">
-                  <div class="mask mask-circle w-7 lg:w-12 md:w-14 sm:w-8">
+                  <div class="mask mask-circle w-10 lg:w-14 md:w-14 sm:w-8">
                     <img src="{{ asset('icon/anime.png') }}" />
                   </div>
-                  <h5 class="font-bold ml-5">Adent</h5>
+                  <div class="flex flex-col gap-2">
+                    <h5 class="font-bold mt-4 ml-5 h-4">Adent</h5>
+                    <span class="text-xs ml-5">10.00</span>
+                  </div>
                 </div>
                 <p class="mb-1 font-semibold">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Similique, laudantium?
+                  Similique, laudantium
                 </p>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div class="w-full max-w-full px-3 lg:w-5/12 lg:flex-none" id="youranswer">
-      <div
-        class="shadow-soft-xl relative flex min-w-0 flex-col break-words rounded-2xl border-0 border-solid bg-white bg-clip-border p-4"
-      >
-        <div class="relative h-full overflow-hidden bg-cover rounded-xl">
-          <div class="bg-white p-5 rounded-2xl relative">
-            <h2
-              class="text-md lg:text-xl md:text-xl sm:text-md font-bold mb-4 flex items-center justify-center"
-            >
-              Your Answer
-            </h2>
-            <hr class="h-0.5 bg-slate-400" />
-            <form role="form text-left" id="editor">
-              <!-- <div class="mb-4">
-                <label for="" class="font-bold"></label>
-                <textarea
-                  class="bg-Background mt-2 text-sm w-full rounded-lg border border-solid border-gray-300 py-2 px-3"
-                  name=""
-                  id=""
-                  cols="30"
-                  rows="10"
-                ></textarea>
-              </div> -->
-            </form>
-            <div class="flex justify-end gap-2 mt-5">
-              <button
-                class="bg-Blue text-white rounded-xl lg:px-10 md:px-7 sm:px-5 px-4 py-1"
+      <!-- 3 -->
+      <div class="w-full mt-4 max-w-full lg:flex-none" id="youranswer">
+        <div
+          class="shadow-soft-xl relative flex min-w-0 flex-col break-words rounded-2xl border-0 border-solid bg-white bg-clip-border p-4"
+        >
+          <div class="relative h-full overflow-hidden bg-cover rounded-xl">
+            <div class="bg-white p-5 rounded-2xl relative">
+              <h2
+                class="text-md lg:text-xl md:text-xl sm:text-md font-bold mb-4 flex items-center justify-center"
               >
-                Answer
-              </button>
+                Your Answer
+              </h2>
+              <hr class="h-0.5 bg-slate-400" />
+              <form role="form text-left" id="editor"></form>
+              <div class="flex justify-end gap-2 mt-5">
+                <button
+                  class="bg-Blue text-white rounded-xl lg:px-10 md:px-7 sm:px-5 px-4 py-1"
+                >
+                  Answer
+                </button>
+              </div>
             </div>
           </div>
         </div>
