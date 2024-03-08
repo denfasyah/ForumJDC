14d575cabb611ece810ea3d533563907f100283e
denfasyah <denfasyah@gmail.com>
2024-03-08 18:06:26 +0700

    set widht img detail event page && detail job page

--- resources/views/event/detail/index.blade.php
+++ resources/views/event/detail/index.blade.php
@@ -1,8 +1,8 @@
 @extends('index') @section('event')
 
 <div class="container mt-5">
-  <div class="flex px-10 flex-wrap mt-6 -mx-3">
-    <div class="w-full max-w-full px-3 lg:w-5/12 lg:flex-none">
+  <div class="flex flex-wrap mt-6 -mx-3">
+    <div class="w-full max-w-full px-3 lg:w-1/2 lg:flex-none">
       <h1>Meet Up</h1>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit sapiente
@@ -15,11 +15,13 @@
       <p>Time : 27 Februari 2024</p>
     </div>
 
-    <div class="w-full px-3 lg:mt-0 mb-6 lg:mb-0 lg:w-7/12 lg:flex-none">
+    <div
+      class="w-full px-3 lg:mt-0 mb-6 flex justify-center lg:mb-0 lg:w-1/3 lg:flex-none"
+    >
       <img
         src="{{ asset('img/meetup.png') }}"
         alt="img-blur-shadow"
-        class="w-full rounded-2xl"
+        class="w-80 rounded-2xl"
       />
     </div>
   </div>
--- resources/views/job/detail/index.blade.php
+++ resources/views/job/detail/index.blade.php
@@ -1,8 +1,8 @@
 @extends('index') @section('job')
 
 <div class="container mt-5">
-  <div class="flex px-10 flex-wrap mt-6 -mx-3">
-    <div class="w-full max-w-full px-3 lg:w-5/12 lg:flex-none">
+  <div class="flex flex-wrap mt-6 -mx-3">
+    <div class="w-full max-w-full px-3 lg:w-1/2 lg:flex-none">
       <h1>Lorem</h1>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit sapiente
@@ -12,14 +12,16 @@
         Illo, voluptatem, autem corrupti dolores fuga asperiores deserunt
         corporis repellendus quos minima unde incidunt.
       </p>
-      <p>https://www.example.com/loker_it/</p>
+      <a href="">https://www.example.com/loker_it/</a>
     </div>
 
-    <div class="w-full px-3 lg:mt-0 mb-6 lg:mb-0 lg:w-7/12 lg:flex-none">
+    <div
+      class="w-full px-3 lg:mt-0 mb-6 flex justify-center lg:mb-0 lg:w-1/3 lg:flex-none"
+    >
       <img
         src="{{ asset('img/loker1.png') }}"
         alt="img-blur-shadow"
-        class="w-full rounded-t-2xl h-full"
+        class="w-80 rounded-t-2xl h-full"
       />
     </div>
   </div>
